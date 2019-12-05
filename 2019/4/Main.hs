49ae0f543a94feb884ceba73962c4fb50bbd3012
Kaleb Smart <kaleb.smart@adtran.com>
2019-12-05 12:32:20 -0600

    refactor problem 4 part 1

--- 2019/4/Main.hs
+++ 2019/4/Main.hs
@@ -3,12 +3,7 @@ import Data.Char
 import Data.List
 
 containsDouble :: String -> Bool
-containsDouble [] = False
-containsDouble [_] = False
-containsDouble (a:xs)
-    | a == (head xs) = True
-    | otherwise = tailContainsDouble
-    where tailContainsDouble = containsDouble xs
+containsDouble = not . null . filter (>1) . map length . group 
 
 isNotDecreasing :: String -> Bool
 isNotDecreasing [] = True
@@ -29,9 +24,9 @@ range input = [start..stop]
         where [start, stop] = map read $ splitOn "-" input :: [Int]
 
 solve :: String -> Int
-solve input = length $ validPasswords $ range input
+solve = length . validPasswords . range
 
 main = do
     input <- readFile "input.txt"
-    let answer = solve $ input
+    let answer = solve input
     putStrLn $ show answer
