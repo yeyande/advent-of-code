b982c2efa34fc01dc64195ad23847b4f5d92980f
Kaleb Smart <kaleb.smart@adtran.com>
2019-12-30 16:13:25 -0600

    solve day 8 part 2

--- 2019/08/Main.hs
+++ 2019/08/Main.hs
@@ -1,19 +1,41 @@
 import Data.List.Split
 import Control.Exception
 import Data.List
-type Layer = [Int]
+data Pixel = Transparent | Black | White deriving (Show)
+type Layer = [[Pixel]]
 type Image = [Layer]
 
-testData = "123456789012"
+testData = "0222112222120000"
 
 layerWidth = 25
 layerHeight = 6
 
-solve :: String -> Int -> Int -> Int
-solve input width height = (numberOfOccurrences '1' leastZeroes) * (numberOfOccurrences '2' leastZeroes)
+solve :: String -> Int -> Int -> [String]
+solve input width height = toDisplayCharacters $ merge layers
     where layerLength = width * height
-          layers = chunksOf layerLength input
-          leastZeroes = snd $ minimumBy (\a b -> compare (fst a) (fst b)) $ map (\layer -> (numberOfOccurrences  '0' layer, layer)) layers
+          layers = parseLayers $ map (chunksOf width) $ chunksOf layerLength input
+ 
+parseLayers :: [[String]] -> Image
+parseLayers layers = map (map (map toPixel)) layers
+
+toPixel :: Char -> Pixel
+toPixel '0' = Black
+toPixel '1' = White
+toPixel '2' = Transparent
+
+merge :: Image -> Layer
+merge img = map (map (foldl1 resolvePixel)) $ map transpose $ transpose img
+
+toDisplayCharacters :: Layer -> [String]
+toDisplayCharacters layer = map (map pixelToCharacter) layer
+
+pixelToCharacter :: Pixel -> Char
+pixelToCharacter Black = '█'
+pixelToCharacter White = ' '
+
+resolvePixel :: Pixel -> Pixel -> Pixel
+resolvePixel Transparent b = b
+resolvePixel a _ = a
 
 numberOfOccurrences :: Char -> String -> Int
 numberOfOccurrences e xs = length $ findIndices (==e) xs
@@ -21,9 +43,9 @@ numberOfOccurrences e xs = length $ findIndices (==e) xs
 main = do
     input <- readFile "input.txt"
     let answer = solve ((lines input) !! 0) layerWidth layerHeight
-    putStrLn $ show answer
+    putStrLn $ unlines answer
 
-testExample :: Int
+testExample :: [String]
 testExample = assert (got == expected) got
-    where got = solve testData 3 2
-          expected = 1
+    where got = solve testData 2 2
+          expected = ["█ ", " █"]
