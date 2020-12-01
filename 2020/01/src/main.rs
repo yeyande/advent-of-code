6cea6cae31ec3eb2127b5a61320250a3c3d8040e
Kaleb Smart <kalebsma@gmail.com>
2020-12-01 12:20:53 -0600

    solve day 1 part 2....grossly

--- 2020/01/src/main.rs
+++ 2020/01/src/main.rs
@@ -4,25 +4,24 @@ fn main() {
     let contents = fs::read_to_string("input.txt").unwrap();
     let values : Vec<i32> = contents
         .lines().into_iter().map(|x| x.parse().unwrap()).collect();
-    let (x, y) = get_pairs_with_sum(values, 2020);
-    println!("{}", x*y);
+    let (x, y, z) = get_pairs_with_sum(values, 2020);
+    println!("{}", x*y*z);
 }
 
-fn get_pairs_with_sum(haystack: Vec<i32>, needle: i32) -> (i32, i32) {
+fn get_pairs_with_sum(haystack: Vec<i32>, needle: i32) -> (i32, i32, i32) {
     let mut sorted: Vec<i32> = haystack.clone();
     sorted.sort();
-    let (left, right): (Vec<i32>, Vec<i32>) = sorted.iter().partition(|&&x| x < needle/2);
-    let mut lesser: i32 = 0;
-    let mut greater: i32 = 0;
-    for x in left {
-        for y in right.clone() {
-            if x + y == needle {
-                lesser = x;
-                greater = y;
+    let mut answer: (i32, i32, i32) = (0, 0, 0);
+    for x in sorted.clone() {
+        for y in sorted.clone()[1..].to_vec() {
+            for z in sorted.clone()[2..].to_vec() {
+                if x + y + z == needle {
+                    answer = (x, y, z)
+                }
             }
         }
     }
-    return (greater, lesser)
+    return answer;
 }
 
 #[cfg(test)]
@@ -31,6 +30,6 @@ mod tests {
 
     #[test]
     fn works_on_sample_input() {
-        assert_eq!(get_pairs_with_sum(vec![1721, 979, 366, 299, 675, 1456], 2020), (1721, 299))
+        assert_eq!(get_pairs_with_sum(vec![1721, 979, 366, 299, 675, 1456], 2020), (979, 366, 675))
     }
 }
