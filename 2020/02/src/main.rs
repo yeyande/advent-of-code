ff2c9fa4c3dea932389b53bc1591b4a1d83776ea
Kaleb Smart <kalebsma@gmail.com>
2020-12-02 12:58:11 -0600

    solve day 2 part 2

--- 2020/02/src/main.rs
+++ 2020/02/src/main.rs
@@ -1,6 +1,5 @@
 #[macro_use] extern crate lazy_static;
 use regex::Regex;
-use std::cmp::Ordering;
 
 fn main() {
     let contents = include_str!("../input.txt");
@@ -14,18 +13,22 @@ fn parse_password(password: &str) -> Option<String> {
     }
     let captures : Vec<String> = FORMAT_PARSER.captures(password).unwrap()
         .iter().map(|x| String::from(x.unwrap().as_str())).collect();
-    let min: usize = captures[1].parse().unwrap();
-    let max: usize = captures[2].parse().unwrap();
-    let (letter, password) = (&captures[3], &captures[4]);
-    let m = password.matches(letter).count();
-    match m.cmp(&min) {
-        Ordering::Greater | Ordering::Equal => {
-            match m.cmp(&max) {
-                Ordering::Less | Ordering::Equal => Some(String::from(password)),
-                _ => None
+    let pos_1: usize = captures[1].parse().unwrap();
+    let pos_2: usize = captures[2].parse().unwrap();
+    let (letter, password) = (captures[3].chars().next().unwrap(), &captures[4]);
+    match password.chars().nth(pos_1 - 1) {
+        Some(a) => match password.chars().nth(pos_2 - 1) {
+            Some(b) => {
+                match (letter == a) ^ (letter == b) {
+                    true => {
+                        Some(String::from(password))
+                    },
+                    false => None
+                }
             }
+            None => None
         }
-        _ => None
+        None => None
     }
 }
 
@@ -40,6 +43,6 @@ mod tests {
 
     #[test]
     fn works_on_sample_input() {
-        assert_eq!(get_valid_password_count(vec!["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc", "1-6 c: xcccxcccccz"]), 2)
+        assert_eq!(get_valid_password_count(vec!["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc", "1-6 c: xcccxcccccz"]), 1)
     }
 }
