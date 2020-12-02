c25fe42a2197372cff55692c3fab65a31d31bf4a
Kaleb Smart <kalebsma@gmail.com>
2020-12-02 08:28:36 -0600

    solve day 2 part 1

--- 2020/02/src/main.rs
+++ 2020/02/src/main.rs
@@ -1,9 +1,37 @@
+#[macro_use] extern crate lazy_static;
+use regex::Regex;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = include_str!("../input.txt");
+    let passwords : Vec<&str> = contents.lines().into_iter().collect();
+    println!("{}", get_valid_password_count(passwords));
+}
+
+fn parse_password(password: &str) -> Option<String> {
+    lazy_static! {
+        static ref FORMAT_PARSER: Regex = Regex::new(r"([\d]+)-([\d]+) (\w): (\w+)").unwrap();
+    }
+    let captures : Vec<String> = FORMAT_PARSER.captures(password).unwrap()
+        .iter().map(|x| String::from(x.unwrap().as_str())).collect();
+    let min: usize = captures[1].parse().unwrap();
+    let max: usize = captures[2].parse().unwrap();
+    let (letter, password) = (&captures[3], &captures[4]);
+    let m = password.matches(letter).count();
+    match m.cmp(&min) {
+        Ordering::Greater | Ordering::Equal => {
+            match m.cmp(&max) {
+                Ordering::Less | Ordering::Equal => Some(String::from(password)),
+                _ => None
+            }
+        }
+        _ => None
+    }
 }
 
 fn get_valid_password_count(passwords: Vec<&str>) -> usize {
-    return passwords.len();
+    let valid_passwords: Vec<String> = passwords.iter().filter_map(|x| parse_password(x)).collect();
+    return valid_passwords.len();
 }
 
 #[cfg(test)]
@@ -12,6 +40,6 @@ mod tests {
 
     #[test]
     fn works_on_sample_input() {
-        assert_eq!(get_valid_password_count(vec!["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]), 2)
+        assert_eq!(get_valid_password_count(vec!["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc", "1-6 c: xcccxcccccz"]), 2)
     }
 }
