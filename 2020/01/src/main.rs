2c79029c7e11ffcb253b3893aa3fdc6b88981772
Kaleb Smart <kalebsma@gmail.com>
2020-12-01 18:58:39 -0600

    make it better... sort of

--- 2020/01/src/main.rs
+++ 2020/01/src/main.rs
@@ -4,24 +4,37 @@ fn main() {
     let contents = fs::read_to_string("input.txt").unwrap();
     let values : Vec<i32> = contents
         .lines().into_iter().map(|x| x.parse().unwrap()).collect();
-    let (x, y, z) = get_pairs_with_sum(values, 2020);
+    let (x, y, z) = get_triple_with_sum(values, 2020).unwrap();
     println!("{}", x*y*z);
 }
 
-fn get_pairs_with_sum(haystack: Vec<i32>, needle: i32) -> (i32, i32, i32) {
+fn get_pairs_with_sum(haystack: Vec<i32>, needle: i32) -> Option<(i32, i32)> {
     let mut sorted: Vec<i32> = haystack.clone();
     sorted.sort();
-    let mut answer: (i32, i32, i32) = (0, 0, 0);
     for x in sorted.clone() {
         for y in sorted.clone()[1..].to_vec() {
-            for z in sorted.clone()[2..].to_vec() {
-                if x + y + z == needle {
-                    answer = (x, y, z)
-                }
+            if x + y == needle {
+                return Some((x, y));
             }
         }
     }
-    return answer;
+    None
+}
+
+fn get_triple_with_sum(haystack: Vec<i32>, needle: i32) -> Option<(i32, i32, i32)> {
+    let mut sorted: Vec<i32> = haystack.clone();
+    sorted.sort();
+    for x in sorted.clone() {
+        match get_pairs_with_sum(sorted.clone(), needle-x) {
+            Some((y, z)) => {
+                if x + y + z == needle {
+                    return Some((x, y, z))
+                }
+            }
+            None => continue
+        }
+    }
+    None
 }
 
 #[cfg(test)]
@@ -30,6 +43,6 @@ mod tests {
 
     #[test]
     fn works_on_sample_input() {
-        assert_eq!(get_pairs_with_sum(vec![1721, 979, 366, 299, 675, 1456], 2020), (979, 366, 675))
+        assert_eq!(get_triple_with_sum(vec![1721, 979, 366, 299, 675, 1456], 2020).unwrap(), (366, 675, 979))
     }
 }
