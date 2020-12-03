15fe634caba6ce445cf9c5ccc6d7131e85728902
Kaleb Smart <kalebsma@gmail.com>
2020-12-03 07:57:12 -0600

    solve day 3 part 1

--- 2020/03/src/main.rs
+++ 2020/03/src/main.rs
@@ -1,3 +1,48 @@
 fn main() {
-    println!("Hello, world!");
+    let contents = include_str!("../input.txt");
+    let map : Vec<&str> = contents.lines().into_iter().collect();
+    println!("{}", get_trees_hit_down_slope(map, (3,1)));
+}
+
+fn get_trees_hit_down_slope(map: Vec<&str>, slope: (usize, usize)) -> usize {
+    let mut pos : (usize, usize) = (0, 0);
+    let modulo: usize = map[0].len();
+    let (run, rise) = slope;
+    let mut trees_hit = 0;
+    while pos.1 < map.len() - 1 {
+        let (x, y) = pos;
+        pos = ((x+run) % modulo, y+rise);
+        if map[pos.1].chars().nth(pos.0) == Some('#') {
+            trees_hit += 1;
+        }
+    }
+    return trees_hit;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn works_on_sample_input() {
+        assert_eq!(
+            get_trees_hit_down_slope(
+                vec![
+                    "..##.......",
+                    "#...#...#..",
+                    ".#....#..#.",
+                    "..#.#...#.#",
+                    ".#...##..#.",
+                    "..#.##.....",
+                    ".#.#.#....#",
+                    ".#........#",
+                    "#.##...#...",
+                    "#...##....#",
+                    ".#..#...#.#",
+                ],
+                (3,1)
+                ),
+            7
+        )
+    }
 }
