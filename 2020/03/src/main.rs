96126121f89b83f817a658781924988b79ff5933
Kaleb Smart <kalebsma@gmail.com>
2020-12-03 08:21:13 -0600

    solve day 3 part 2

--- 2020/03/src/main.rs
+++ 2020/03/src/main.rs
@@ -1,17 +1,21 @@
 fn main() {
     let contents = include_str!("../input.txt");
-    let map : Vec<&str> = contents.lines().into_iter().collect();
-    println!("{}", get_trees_hit_down_slope(map, (3,1)));
+    let map: Vec<&str> = contents.lines().into_iter().collect();
+    let result = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+        .iter()
+        .map(|&x| get_trees_hit_down_slope(map.clone(), x))
+        .fold(1, |a, b| a * b);
+    println!("{}", result);
 }
 
 fn get_trees_hit_down_slope(map: Vec<&str>, slope: (usize, usize)) -> usize {
-    let mut pos : (usize, usize) = (0, 0);
+    let mut pos: (usize, usize) = (0, 0);
     let modulo: usize = map[0].len();
     let (run, rise) = slope;
     let mut trees_hit = 0;
     while pos.1 < map.len() - 1 {
         let (x, y) = pos;
-        pos = ((x+run) % modulo, y+rise);
+        pos = ((x + run) % modulo, y + rise);
         if map[pos.1].chars().nth(pos.0) == Some('#') {
             trees_hit += 1;
         }
@@ -23,26 +27,40 @@ fn get_trees_hit_down_slope(map: Vec<&str>, slope: (usize, usize)) -> usize {
 mod tests {
     use super::*;
 
-    #[test]
-    fn works_on_sample_input() {
-        assert_eq!(
-            get_trees_hit_down_slope(
-                vec![
-                    "..##.......",
-                    "#...#...#..",
-                    ".#....#..#.",
-                    "..#.#...#.#",
-                    ".#...##..#.",
-                    "..#.##.....",
-                    ".#.#.#....#",
-                    ".#........#",
-                    "#.##...#...",
-                    "#...##....#",
-                    ".#..#...#.#",
-                ],
-                (3,1)
-                ),
-            7
-        )
+    macro_rules! input_tests {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (input, expected) = $value;
+                assert_eq!(
+                    expected,
+                    get_trees_hit_down_slope(
+                        vec![
+                            "..##.......",
+                            "#...#...#..",
+                            ".#....#..#.",
+                            "..#.#...#.#",
+                            ".#...##..#.",
+                            "..#.##.....",
+                            ".#.#.#....#",
+                            ".#........#",
+                            "#.##...#...",
+                            "#...##....#",
+                            ".#..#...#.#",
+                        ],
+                        input
+                    )
+                );
+            }
+        )*
+        }
+    }
+    input_tests! {
+        test_1: ((3,1), 7),
+        test_2: ((1,1), 2),
+        test_3: ((5,1), 3),
+        test_4: ((7,1), 4),
+        test_5: ((1,2), 2),
     }
 }
