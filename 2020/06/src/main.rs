9e979bd0a524185281c12643754b159310797842
Kaleb Smart <kalebsma@gmail.com>
2020-12-06 11:20:04 -0600

    solve day 6 part 2

--- 2020/06/src/main.rs
+++ 2020/06/src/main.rs
@@ -3,26 +3,37 @@ use itertools::Itertools;
 fn main() {
     let contents = include_str!("../input.txt");
     let batches: Vec<&str> = contents.lines().into_iter().collect();
-    let solution: usize = get_answers(batches).iter().fold(0, |a, b| a + b);
+    let solution: usize = get_answers(batches).iter().sum();
     println!("{}", solution)
 }
 
 fn get_answers(answers: Vec<&str>) -> Vec<usize> {
-    let groups : Vec<String> = answers
+    let answer_values: Vec<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
+    let groups: Vec<Vec<String>> = answers
         .into_iter()
         .group_by(|x| *x != "")
         .into_iter()
         .filter(|(matched, _)| *matched)
-        .map(|(_, mut x)| String::from(x.join("")))
+        .map(|(_, x)| x.map(String::from).collect())
         .collect();
-    groups.iter().map(|x| {
-        let mut chars: Vec<char> = x.chars().collect();
-        chars.sort();
-        chars.dedup();
-        chars.len()
-    }).collect()
+    groups
+        .iter()
+        .map(|x| {
+            let respondents = x.len();
+            answer_values
+                .clone()
+                .into_iter()
+                .filter_map(|c| {
+                    let matches: Vec<usize> = x.iter().filter_map(|a| a.find(c)).collect();
+                    match matches.len() == respondents {
+                        true => Some(1),
+                        false => None,
+                    }
+                })
+                .sum()
+        })
+        .collect()
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -30,22 +41,11 @@ mod tests {
 
     #[test]
     fn should_work_for_sample_input() {
-        assert_eq!(get_answers(vec![
-                               "abc",
-                               "",
-                               "a",
-                               "b",
-                               "c",
-                               "",
-                               "ab",
-                               "ac",
-                               "",
-                               "a",
-                               "a",
-                               "a",
-                               "a",
-                               "",
-                               "b"]
-                               ), [3, 3, 3, 1, 1]);
+        assert_eq!(
+            get_answers(vec![
+                "abc", "", "a", "b", "c", "", "ab", "ac", "", "a", "a", "a", "a", "", "b"
+            ]),
+            [3, 0, 1, 1, 1]
+        );
     }
 }
