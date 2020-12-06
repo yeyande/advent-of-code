ee0c5fb99f1276acd69d78a9ec7af2d649ef7264
Kaleb Smart <kalebsma@gmail.com>
2020-12-06 10:04:16 -0600

    solve day 6 part 1

--- 2020/06/src/main.rs
+++ 2020/06/src/main.rs
@@ -1,3 +1,51 @@
+use itertools::Itertools;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = include_str!("../input.txt");
+    let batches: Vec<&str> = contents.lines().into_iter().collect();
+    let solution: usize = get_answers(batches).iter().fold(0, |a, b| a + b);
+    println!("{}", solution)
+}
+
+fn get_answers(answers: Vec<&str>) -> Vec<usize> {
+    let groups : Vec<String> = answers
+        .into_iter()
+        .group_by(|x| *x != "")
+        .into_iter()
+        .filter(|(matched, _)| *matched)
+        .map(|(_, mut x)| String::from(x.join("")))
+        .collect();
+    groups.iter().map(|x| {
+        let mut chars: Vec<char> = x.chars().collect();
+        chars.sort();
+        chars.dedup();
+        chars.len()
+    }).collect()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_work_for_sample_input() {
+        assert_eq!(get_answers(vec![
+                               "abc",
+                               "",
+                               "a",
+                               "b",
+                               "c",
+                               "",
+                               "ab",
+                               "ac",
+                               "",
+                               "a",
+                               "a",
+                               "a",
+                               "a",
+                               "",
+                               "b"]
+                               ), [3, 3, 3, 1, 1]);
+    }
 }
