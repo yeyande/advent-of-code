efc1af412be1794adb5bdf1c0329896548d7aad0
Kaleb Smart <kalebsma@gmail.com>
2020-12-07 17:06:06 -0600

    more progress

--- 2020/07/src/main.rs
+++ 2020/07/src/main.rs
@@ -4,19 +4,26 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn parse_rule(rule: &str) -> (&str, Vec<(usize, &str)>) {
-    let key_bag, bag_connections = rule.split(" contain ")[0..1];
-    let key: &str = key_bag.split(" bags")[0];
-    let connections: Vec<(usize, &str)> = bag_connections.split(", ").map(|bag| 
-            let count, bag_definition = bag.splitn(1, " ");
-            let bag = bag_definition.replace("bag"
-                                                 ).collect();
+fn parse_rule(rule: &str) -> (String, Vec<(usize, &str)>) {
+    let split: Vec<&str> = rule.split(" contain ").collect();
+    let (key_bag, bag_connections) = (split[0], split[1]);
+    let key: String = key_bag.replace(" bags", "");
+    let connections: Vec<(usize, &str)> = bag_connections.split(", ").collect::<Vec<&str>>().iter().map(|bag|
+            let bag_info: Vec<&str> = bag.splitn(1, " ").collect(),
+            let (count, connection): (usize, &str) = (bag_info[0].parse().unwrap(), bag_info[1]);
+
+            //let bag = bag_definition.replace("bags", "").replace("bag", "").replace(".", "");
+            (1, *connection)
+             ).collect();
     return (key, connections)
 }
 
-fn parse_rules(rules: Vec<&str>) -> HashMap<&str, Vec<(usize, &str)>> {
-    let graph: HashMap<&str, Vec<(usize, &str)>> = HashMap::new();
-    rules.map(|rule| graph.insert(parse_rule)).collect();
+fn parse_rules(rules: Vec<&str>) -> HashMap<String, Vec<(usize, &str)>> {
+    let mut graph: HashMap<String, Vec<(usize, &str)>> = HashMap::new();
+    for rule in rules.iter() {
+        let (key, value) = parse_rule(rule);
+        graph.insert(key, value);
+    }
     graph
 }
 
@@ -26,16 +33,16 @@ mod tests {
 
     #[test]
     fn should_parse_rules_correctly() {
-        let expected: HashMap<&str, Vec<(usize, &str)>> = vec![
-                ("light red", vec![(1, "bright white"), (2, "muted yellow")]),
-                ("dark orange", vec![(3, "bright white"), (4, "muted yellow")]),
-                ("bright white", vec![(1, "shiny gold")]),
-                ("muted yellow", vec![(2, "shiny gold"), (9, "faded blue")]),
-                ("shiny gold", vec![(1, "dark olive"), (2, "vibrant plum")]),
-                ("dark olive", vec![(3, "faded blue"), (4, "dotted black")]),
-                ("vibrant plum", vec![(5, "faded blue"), (6, "dotted black")]),
-                ("faded blue", vec![]),
-                ("dotted black", vec![]),
+        let expected: HashMap<String, Vec<(usize, &str)>> = vec![
+                ("light red".to_string(), vec![(1, "bright white"), (2, "muted yellow")]),
+                ("dark orange".to_string(), vec![(3, "bright white"), (4, "muted yellow")]),
+                ("bright white".to_string(), vec![(1, "shiny gold")]),
+                ("muted yellow".to_string(), vec![(2, "shiny gold"), (9, "faded blue")]),
+                ("shiny gold".to_string(), vec![(1, "dark olive"), (2, "vibrant plum")]),
+                ("dark olive".to_string(), vec![(3, "faded blue"), (4, "dotted black")]),
+                ("vibrant plum".to_string(), vec![(5, "faded blue"), (6, "dotted black")]),
+                ("faded blue".to_string(), vec![]),
+                ("dotted black".to_string(), vec![]),
             ].into_iter().collect();
         assert_eq!(
             parse_rules(vec![
