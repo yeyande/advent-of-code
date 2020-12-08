681d26a4485a9227e7a428a107254de7a7d19bf4
Kaleb Smart <kalebsma@gmail.com>
2020-12-08 11:15:30 -0600

    solve day 8 part 2

--- 2020/08/src/main.rs
+++ 2020/08/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     println!("{}", solution)
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum Opcode {
     NOP,
     ACC,
@@ -18,7 +18,7 @@ struct Application {
     program: Vec<(Opcode, i32)>,
     executed: Vec<i32>,
     program_counter: i32,
-    halted: bool
+    loop_detected: bool
 }
 
 impl Application {
@@ -28,21 +28,25 @@ impl Application {
             program: program,
             executed: vec![],
             program_counter: 0,
-            halted: false,
+            loop_detected: false,
         }
     }
 
     pub fn run(&mut self) {
         loop {
             match self.executed.contains(&self.program_counter) {
-                true => self.halted = true,
+                true => self.loop_detected = true,
                 false => self.executed.push(self.program_counter)
             };
-            match self.halted {
+            match self.loop_detected {
                 true => return,
                 false => {
-                    let (opcode, arg) = self.program[self.program_counter as usize];
-                    self.step(&opcode, arg)
+                    let copy = self.program.clone();
+                    let next_opcode = copy.get(self.program_counter as usize).clone();
+                    match next_opcode {
+                        Some((opcode, arg)) => self.step(&opcode, *arg),
+                        None => return
+                    }
                 }
             }
         }
@@ -70,11 +74,37 @@ fn parse_instruction(instruction: &str) -> Option<(Opcode, i32)> {
     Some((opcode, arg))
 }
 
-fn execute(code: Vec<&str>) -> i32 {
-    let operations: Vec<(Opcode, i32)> = code.iter().filter_map(|x| parse_instruction(*x)).collect();
+fn get_valid_program_result(operations: Vec<(Opcode, i32)>) -> Option<i32> {
     let mut application = Application::new(operations);
     application.run();
-    application.accumulator
+    match application.loop_detected {
+        false => Some(application.accumulator),
+        true => None,
+    }
+}
+
+fn execute(code: Vec<&str>) -> i32 {
+    let operations: Vec<(Opcode, i32)> = code.iter().filter_map(|x| parse_instruction(*x)).collect();
+    let enumerated_repairs = enumerate_repairs(operations.clone());
+    let valid_results: Vec<i32> = enumerated_repairs.into_iter().filter_map(get_valid_program_result).collect();
+    valid_results[0]
+}
+
+fn enumerate_repairs(operations: Vec<(Opcode, i32)>) -> Vec<Vec<(Opcode, i32)>> {
+    let mut repairs: Vec<Vec<(Opcode, i32)>> = vec![operations.clone()];
+    let instructions_to_swap: Vec<usize> = operations.clone().into_iter().enumerate().filter(|(_, (op, _))| *op != Opcode::ACC).map(|(idx, _)| idx).collect();
+    for idx in instructions_to_swap.iter() {
+        let mut repaired = operations.clone();
+        let (bad_op, arg) = repaired[*idx];
+        let new_op = match bad_op {
+            Opcode::NOP => Opcode::JMP,
+            Opcode::JMP => Opcode::NOP,
+            Opcode::ACC => Opcode::ACC,
+        };
+        repaired[*idx] = (new_op, arg);
+        repairs.push(repaired.clone());
+    }
+    repairs
 }
 
 #[cfg(test)]
@@ -95,7 +125,7 @@ mod tests {
                 "jmp -4",
                 "acc +6",
             ]),
-            5
+            8
         );
     }
 }
