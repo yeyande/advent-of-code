d16ced554e3cdbe100e85a40ad6fb57c3706cc16
Kaleb Smart <kalebsma@gmail.com>
2020-12-15 06:57:33 -0600

    solve day 14 part 2

--- 2020/14/src/main.rs
+++ 2020/14/src/main.rs
@@ -8,6 +8,8 @@ fn main() {
     let program: Vec<&str> = contents.lines().into_iter().collect();
     let solution = solve(program.clone());
     println!("{}", solution);
+    let solution_2 = solve_2(program.clone());
+    println!("{}", solution_2);
 }
 
 fn parse_mask(mask: &str) -> (u64, u64) {
@@ -42,6 +44,56 @@ fn solve(program: Vec<&str>) -> u64 {
     memory.values().sum()
 }
 
+fn solve_2(program: Vec<&str>) -> u64 {
+    let mut memory: HashMap<u64, u64> = HashMap::new();
+    let mut mask: &str = "";
+    for line in program.iter() {
+        let split: Vec<&str>= line.split(" = ").collect();
+        let (instruction, value): (&str, &str) = (split[0], split[1]);
+        match instruction {
+            "mask" => mask = value.clone(),
+            _ => {
+                lazy_static! {
+                    static ref MEMORY_PARSER: Regex = Regex::new(r"mem\[(\d+)\]").unwrap();
+                }
+                match MEMORY_PARSER.captures(instruction) {
+                    Some(address) => {
+                        for addr in enumerate_addresses(&mask_address(&address[1], mask)).iter() {
+                            memory.insert(*addr, value.parse().unwrap());
+                        }
+                    }
+                    None => ()
+                };
+            }
+        }
+    }
+    memory.values().sum()
+}
+
+fn enumerate_addresses(address: &str) -> Vec<u64> {
+    match u64::from_str_radix(address, 2).ok() {
+        Some(addr) => vec![addr],
+        None => {
+            let replaced = (address.replacen("X", "1", 1), address.replacen("X", "0", 1));
+            let mut first = enumerate_addresses(&replaced.0);
+            let second = enumerate_addresses(&replaced.1);
+            first.extend(second);
+            first
+        }
+    }
+}
+
+fn mask_address(address: &str, mask: &str) -> String {
+    let parsed: String = format!("{:036b}", address.parse::<u64>().unwrap());
+    mask.chars().zip(parsed.chars()).map(|(m, a)|
+        match m {
+            'X' => 'X',
+            '1' => '1',
+            _ => a
+        }
+    ).collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -74,6 +126,24 @@ mod tests {
     }
 
     #[test]
+    fn enumerate_addresses_should_work() {
+        let mask = "000000000000000000000000000000X1001X";
+        assert_eq!(
+            enumerate_addresses(mask),
+            vec![0b110011, 0b110010, 0b10011, 0b10010]
+        );
+    }
+
+    #[test]
+    fn mask_address_should_work() {
+        let mask = "000000000000000000000000000000X1001X";
+        assert_eq!(
+            mask_address("42", mask),
+            "000000000000000000000000000000X1101X"
+        );
+    }
+
+    #[test]
     fn solve_should_work() {
         assert_eq!(
             solve(vec![
@@ -83,6 +153,19 @@ mod tests {
                 "mem[8] = 0",
             ]),
             165
+        );
+    }
+
+    #[test]
+    fn solve_2_should_work() {
+        assert_eq!(
+            solve_2(vec![
+                "mask = 000000000000000000000000000000X1001X",
+                "mem[42] = 100",
+                "mask = 00000000000000000000000000000000X0XX",
+                "mem[26] = 1",
+            ]),
+            208
         );
     }
 }
