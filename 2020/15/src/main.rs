536d667c194002a4bc954f9b325e80d086a048df
Kaleb Smart <kalebsma@gmail.com>
2020-12-15 10:34:59 -0600

    solve day 15 part 2

--- 2020/15/src/main.rs
+++ 2020/15/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 fn main() {
     let contents = include_str!("../input.txt");
-    let solution = get_nth_spoken_number(contents, 2020);
+    let solution = get_nth_spoken_number(contents, 30000000);
     println!("{}", solution);
 }
 
@@ -15,13 +15,12 @@ fn get_nth_spoken_number(start: &str, n: usize) -> usize {
     }
     let mut last_turn = starting_numbers.len();
     for turn in starting_numbers.len()+1..=n {
-        last_spoken = match database.clone().get(&(last_spoken.clone())) {
+        let previous_last_spoken = last_spoken.clone();
+        last_spoken = match database.get(&(last_spoken.clone())) {
             None => {
-                database.insert(last_spoken.clone(), turn-1);
                 0
             },
             Some(n) => {
-                database.insert(last_spoken.clone(), turn-1);
                 if turn == starting_numbers.len()+1 {
                     0
                 } else {
@@ -29,6 +28,7 @@ fn get_nth_spoken_number(start: &str, n: usize) -> usize {
                 }
             }
         };
+        database.insert(previous_last_spoken, turn-1);
         last_turn = turn;
     }
     last_spoken
@@ -39,15 +39,36 @@ fn get_nth_spoken_number(start: &str, n: usize) -> usize {
 mod tests {
     use super::*;
 
-    #[test]
-    fn get_nth_spoken_number_should_work() {
-        assert_eq!(get_nth_spoken_number("0,3,6", 2020), 436);
-        assert_eq!(get_nth_spoken_number("1,3,2", 2020), 1);
-        assert_eq!(get_nth_spoken_number("2,1,3", 2020), 10);
-        assert_eq!(get_nth_spoken_number("1,2,3", 2020), 27);
-        assert_eq!(get_nth_spoken_number("2,3,1", 2020), 78);
-        assert_eq!(get_nth_spoken_number("3,2,1", 2020), 438);
-        assert_eq!(get_nth_spoken_number("3,1,2", 2020), 1836);
+    macro_rules! input_tests {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let ((start, nth), expected) = $value;
+                assert_eq!(
+                    expected,
+                    get_nth_spoken_number(start, nth)
+                );
+            }
+        )*
+        }
+    }
 
+    input_tests! {
+        test_1: (("0,3,6", 2020), 436),
+        test_2: (("1,3,2", 2020), 1),
+        test_3: (("2,1,3", 2020), 10),
+        test_4: (("1,2,3", 2020), 27),
+        test_5: (("2,3,1", 2020), 78),
+        test_6: (("3,2,1", 2020), 438),
+        test_7: (("3,1,2", 2020), 1836),
+
+        test_08: (("0,3,6", 30000000), 175594),
+        test_09: (("1,3,2", 30000000), 2578),
+        test_10: (("2,1,3", 30000000), 3544142),
+        test_11: (("1,2,3", 30000000), 261214),
+        test_12: (("2,3,1", 30000000), 6895259),
+        test_13: (("3,2,1", 30000000), 18),
+        test_14: (("3,1,2", 30000000), 362),
     }
 }
