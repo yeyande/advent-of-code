e129cf6745ab32069249296449112ae0a117e69d
Kaleb Smart <kalebsma@gmail.com>
2021-12-01 08:38:13 -0500

    Solve day 01 part 02

--- 2021/01/Main.hs
+++ 2021/01/Main.hs
@@ -1,12 +1,26 @@
-depthHasIncreased :: (Int, Int) -> Bool
+depthHasIncreased :: (Ord a) => (a, a) -> Bool
 depthHasIncreased (a, b)
     | a < b = True
     | otherwise = False
+
+getWindow :: (Num a) => [a] -> [a]
+getWindow xs =
+    map (\(a,b,c) -> a+b+c) windows
+    where windows = zip3 xs (drop 1 xs) (drop 2 xs)
+
+getPairs :: [a] -> [(a, a)]
+getPairs xs = zip xs (drop 1 xs)
+
+solve :: (Ord a) => [(a, a)] -> Int
+solve depthPairs = length $ filter (\x -> x) $ map depthHasIncreased depthPairs
 
 main :: IO ()
 main = do
     input <- readFile "input.txt"
     let depths = map read $ lines input :: [Int]
-    let depthPairs = zip depths (drop 1 depths)
+    let depthPairs = getPairs depths
+    let depthWindowPairs = getPairs $ getWindow depths
     
-    putStrLn . show . length $ filter (\x -> x == True) $ map depthHasIncreased depthPairs
+    putStrLn ("Part 1: " ++ (show $ solve depthPairs))
+    putStrLn ("Part 2: " ++ (show $ solve depthWindowPairs))
+
