27a246cf2d2c4f2f4a8970e829fc8c7c61bb0b68
Kaleb Smart <kalebsma@gmail.com>
2021-12-04 09:19:54 -0500

    Solve day 04 part 02

--- 2021/04/Main.hs
+++ 2021/04/Main.hs
@@ -7,19 +7,22 @@ data BingoGame = BingoGame {
                             , getBoards :: [BingoBoard]
                             , getToldNumbers :: [Int]
                             , hasWinner :: Bool
+                            , getWinningBoards :: [BingoBoard]
                             } deriving (Show)
 
 parse :: [String] -> BingoGame
-parse (numbers:boards) = BingoGame (map read $ splitOn "," numbers) (boards') [] False
+parse (numbers:boards) = BingoGame (map read $ splitOn "," numbers) (boards') [] False []
     where boards' = chunksOf 5 $ map (\x -> map read x )$  map (filter (\x -> length x > 0 )) $ map (\x -> splitOn " " x) $ filter (\x -> length x > 0) boards
 
 hasRun :: [Int] -> BingoBoard -> Bool
 hasRun numbers board = any id $ map (all id) $ map (\x -> map (\y -> y `elem` numbers) x) (board ++ (transpose board))
 
 step :: BingoGame -> BingoGame
-step (BingoGame numbers boards toldNumbers _) = BingoGame numbers boards toldNumbers' hasWinner'
+step (BingoGame numbers boards toldNumbers _ winningBoards) = BingoGame numbers boards toldNumbers' hasWinner' (winningBoards ++ winningBoards')
     where toldNumbers' = take (length toldNumbers+1) numbers
           hasWinner' = any id $ map (hasRun toldNumbers') boards
+          winningBoards' = filter (\x -> (not (x `elem` winningBoards)) && (hasRun toldNumbers' x)) boards
+          
 
 solve :: [String] -> Int
 solve l = (last $ getToldNumbers winningGame) * (sum unmarkedNumbers)
@@ -28,7 +31,15 @@ solve l = (last $ getToldNumbers winningGame) * (sum unmarkedNumbers)
           winningBoard = concat $ concat $ filter (hasRun (getToldNumbers winningGame)) (getBoards winningGame)
           unmarkedNumbers = filter (\x -> not $ x `elem` (getToldNumbers winningGame)) winningBoard
 
+solve2 :: [String] -> Int
+solve2 l = (last $ getToldNumbers endedGame) * (sum unmarkedNumbers)
+    where bingo = parse l
+          endedGame = step $ last $ takeWhile (\x -> not (length (getBoards x) == (length (getWinningBoards x)))) $ iterate step bingo
+          losingBoard = last $ getWinningBoards endedGame
+          unmarkedNumbers = filter (\x -> not $ x `elem` (getToldNumbers endedGame)) $ concat losingBoard
+
 main :: IO()
 main = do
     input <- readFile "input.txt"
     putStrLn $ "Part 1: " ++ (show $ solve $ lines input)
+    putStrLn $ "Part 2: " ++ (show $ solve2 $ lines input)
