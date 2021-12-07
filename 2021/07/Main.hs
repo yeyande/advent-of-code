bfa8d7ca24fde74a48d472758efb93ffadb833ec
Kaleb Smart <kalebsma@gmail.com>
2021-12-07 07:46:16 -0500

    Solve day 07 part 02

--- 2021/07/Main.hs
+++ 2021/07/Main.hs
@@ -4,11 +4,20 @@ import Data.List.Split
 parse :: String -> [Int]
 parse s = map read $ splitOn "," s
 
-getCrabMovement :: [Int] -> Int -> (Int, Int)
-getCrabMovement positions destination = (destination, sum $ map (\x-> abs (x-destination)) positions)
+getFuelCost :: [Int] -> Int -> (Int, Int)
+getFuelCost positions destination = (destination, sum $ map (\x-> abs (x-destination)) positions)
 
-solve :: String -> (Int, Int)
-solve s = minimumBy (\(_, fuel) (_, fuel') -> compare fuel fuel') $ map (\x -> getCrabMovement sortedCrabs x) bounds
+getFuelCost2 :: [Int] -> Int -> (Int, Int)
+getFuelCost2 positions destination = (destination, sum $ map (\x-> sum $ map (\y -> abs (x-y)) [(minimum [x, destination])..(maximum [x, destination])]) positions)
+
+solve :: String -> Int
+solve s = snd $ minimumBy (\(_, fuel) (_, fuel') -> compare fuel fuel') $ map (\x -> getFuelCost sortedCrabs x) bounds
+    where sortedCrabs = sort $ parse s
+          bounds = [(minimum sortedCrabs)..(maximum sortedCrabs)]
+
+
+solve2 :: String -> Int
+solve2 s = snd $ minimumBy (\(_, fuel) (_, fuel') -> compare fuel fuel') $ map (\x -> getFuelCost2 sortedCrabs x) bounds
     where sortedCrabs = sort $ parse s
           bounds = [(minimum sortedCrabs)..(maximum sortedCrabs)]
 
@@ -16,3 +25,4 @@ main :: IO()
 main = do
     input <- readFile "input.txt"
     putStrLn $ "Part 1: " ++ (show $ solve input)
+    putStrLn $ "Part 2: " ++ (show $ solve2 input)
